25a14c0712df3f87e5b9ff7ab073147866273ec8
Kristina Lee <kristinalee2000@hotmail.com>
2025-07-16 18:47:54 -0400

    Add edges to graph, Add BFS function
    Fix

--- script.js
+++ script.js
@@ -9,22 +9,62 @@ class Graph {
 
     addEdge(a, b){
         this.adjEdges.get(a).push(b)
-
+        
         // Undirected graph so add edge on both lists
         this.adjEdges.get(b).push(a)
     }
 }
 
 // create board function
-function createBoard(){
-    let board = new Graph()
-    for (let i = 0; i < 8; i++){
-        for (let j = 0; j < 8; j++){
-            board.addVertex([i,j])
-            board.addEdge([i,j],[i+2, j+1])
-        }
+
+let board = new Graph();
+for (let i = 0; i < 8; i++){
+    for (let j = 0; j < 8; j++){
+        board.addVertex(`[${i}, ${j}]`);
     }
-    return board;
 }
 
-console.log(createBoard());
+let possibleX = [-2, -2, -1, -1, 1, 1, 2, 2];
+let possibleY = [-1, 1, -2, 2, -2, 2, -1, 1 ];
+for (let i = 0; i < 8; i++){
+    for (let j = 0; j < 8; j++){
+        for (let index = 0; index < 8; index++){
+            let dx = i + possibleX[index];
+            let dy = j + possibleY[index];
+
+            if (dx > 0 && dx < 8 && dy > 0 && dy < 8 ){
+                board.addEdge(`[${i}, ${j}]`, `[${dx}, ${dy}]`);
+            }
+        }
+    }
+}
+ 
+console.log(board)
+
+function knightMoves (start, end){
+    let visited = {}
+    let queue = [];
+
+    visited[start] = true;
+    queue.push(start);
+    let queueElement;
+
+    while(queueElement !== end) {
+        queueElement = queue.shift();
+
+        console.log(queueElement);
+
+        let getEdges = board.adjEdges.get(queueElement)
+
+        for (let index in getEdges){
+            let adj = getEdges[index];
+            
+            if(!visited[adj]){
+                visited[adj] = true;
+                queue.push(adj)
+            }
+        }
+    }
+}
+
+knightMoves ('[0, 0]', '[3, 3]')
